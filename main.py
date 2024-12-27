363b42f264eb3612f848b17be49ff2dd69c344ab
sepehr <53p3h2@gmail.com>
2024-12-27 17:06:24 +0330

    updating main so it runs torrent and tracker manager

--- main.py
+++ main.py
@@ -1,44 +1,105 @@
 import sys
-import os
-from bcoding import bencode,bdecode
-import hashlib
+from block import State
 import time
-
-#getting and parsing the torrent file
-torrent_file = sys.argv[1]
-with open(torrent_file, 'rb') as file:
-    contents = bdecode(file)
-piece_length = contents['info']['piece length']
-pieces = contents['info']['pieces']
-raw_info_hash = bencode(contents['info'])
-info_hash = hashlib.sha1(raw_info_hash).digest()
-peer_id = hashlib.sha1(str(time.time()).encode('utf-8')).digest()
-announce_list = contents['announce-list'] if 'announce-list' in contents else contents['announce']
-root = contents['info']['name']
-file_names = []
-total_length : int=0
-
-print(contents)
-
-#crating directories for files mentioned in info section
-# if 'files' in contents['info']:
-#     if not os.path.exists(root):
-#         os.mkdir(root,0o0766)
-#     for file in contents['info']['files']:
-#         path_file = os.path.join(root, *file['path'])
-#         if not os.path.exists(os.path.dirname(path_file)):
-#             os.makedirs(os.path.dirname(path_file))
-#         file_names.append({"path" : path_file, "length" : file["length"]})
-#         total_length += file["length"]
-# else:
-#     file_names.append({"path" : root, "length" : contents['info']['length']})
-#     total_length = contents['info']['length']
-
-#getting list of tracker urls
-if 'announce-list' in contents:
-    trackers =(contents['announce-list'])
-else:
-    trackers = (contents['announce'])
+import peers_manager
+import pieces_manager
+import torrent
+import tracker
+import logging
+import os
+import message
 
 
-peer_id = hashlib.sha1(str(time.time()).encode('utf-8')).digest()
+class Run(object):
+    percentage_completed = -1
+    last_log_line = ""
+
+    def __init__(self):
+        try:
+            torrent_file = sys.argv[1]
+        except IndexError:
+            logging.error("No torrent file provided!")
+            sys.exit(0)
+        self.torrent = torrent.Torrent().load_from_path(torrent_file)
+        self.tracker = tracker.Tracker(self.torrent)
+
+        self.pieces_manager = pieces_manager.PiecesManager(self.torrent)
+        self.peers_manager = peers_manager.PeersManager(self.torrent, self.pieces_manager)
+
+        self.peers_manager.start()
+        logging.info("PeersManager Started")
+        logging.info("PiecesManager Started")
+
+    def start(self):
+        peers_dict = self.tracker.get_peers_from_trackers()
+        self.peers_manager.add_peers(peers_dict.values())
+
+        while not self.pieces_manager.all_pieces_completed():
+            if not self.peers_manager.has_unchoked_peers():
+                time.sleep(1)
+                logging.info("No unchocked peers")
+                continue
+
+            for piece in self.pieces_manager.pieces:
+                index = piece.piece_index
+
+                if self.pieces_manager.pieces[index].is_full:
+                    continue
+
+                peer = self.peers_manager.get_random_peer_having_piece(index)
+                if not peer:
+                    continue
+
+                self.pieces_manager.pieces[index].update_block_status()
+
+                data = self.pieces_manager.pieces[index].get_empty_block()
+                if not data:
+                    continue
+
+                piece_index, block_offset, block_length = data
+                piece_data = message.Request(piece_index, block_offset, block_length).to_bytes()
+                peer.send_to_peer(piece_data)
+
+            self.display_progression()
+
+            time.sleep(0.1)
+
+        logging.info("File(s) downloaded successfully.")
+        self.display_progression()
+
+        self._exit_threads()
+
+    def display_progression(self):
+        new_progression = 0
+
+        for i in range(self.pieces_manager.number_of_pieces):
+            for j in range(self.pieces_manager.pieces[i].number_of_blocks):
+                if self.pieces_manager.pieces[i].blocks[j].state == State.FULL:
+                    new_progression += len(self.pieces_manager.pieces[i].blocks[j].data)
+
+        if new_progression == self.percentage_completed:
+            return
+
+        number_of_peers = self.peers_manager.unchoked_peers_count()
+        percentage_completed = float((float(new_progression) / self.torrent.total_length) * 100)
+
+        current_log_line = "Connected peers: {} - {}% completed | {}/{} pieces".format(number_of_peers,
+                                                                                         round(percentage_completed, 2),
+                                                                                         self.pieces_manager.complete_pieces,
+                                                                                         self.pieces_manager.number_of_pieces)
+        if current_log_line != self.last_log_line:
+            print(current_log_line)
+
+        self.last_log_line = current_log_line
+        self.percentage_completed = new_progression
+
+    def _exit_threads(self):
+        self.peers_manager.is_active = False
+        os._exit(0)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+
+    run = Run()
+    run.start()
